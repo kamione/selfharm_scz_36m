893330cd77d73280cc1f151d23de97f3242ac07c
Ting-Yat Wong <yatyat0321@gmail.com>
2022-03-19 20:10:24 -0400

    [add] new baseline variables

--- scripts/02_a_regression.R
+++ scripts/02_a_regression.R
@@ -3,63 +3,190 @@ library(tidyverse)
 library(here)
 library(jtools)
 
-# Data IO  ---------------------------------------------------------------------
-data <- here("data", "processed", "cf_selfharm_longitudinal.csv") %>% 
-    read_csv(col_types = cols()) %>% 
-    select(sh_case, Ageat1stpre, Sex, Filter, Yrs_edu, Age_onset, DUP_days, DUP_DSH_His,
-           DUP_SS_His, sub_abuse, Mortality, mean_36m_pos:mssd_compliance) %>% 
-    na.omit() %>% 
-    mutate(Sex = factor(Sex, levels = c("Male", "Female"), labels = c(1, 2))) %>% 
-    mutate(sh_case = factor(sh_case, levels = c("With Self-harm", "Without Self-harm"), labels = c(1, 0)))
+# page setup for table to docx
+sect_properties <- prop_section(
+    page_size = page_size(orient = "landscape",
+                          width = 7, height = 12),
+    type = "continuous",
+    page_margins = page_mar(gutter = 0)
+)
 
+
+# Data IO  ---------------------------------------------------------------------
+preprocessed_df <- here("data", "processed", "cf_selfharm_longitudinal.rds") %>% 
+    read_rds() %>% 
+    select(sh_case, Ageat1stpre, Sex, Filter, Yrs_edu, Age_onset, DUP_days,
+           DUP_DSH_His, DUP_SS_His, sub_abuse, baseline_sa, Smoker4, Schiz,
+           EP1_hosp, dur_adm1, mean_36m_pos:mssd_compliance)
 
 # Regression -------------------------------------------------------------------
-m0_fit <- data %>% 
-    glm(formula = sh_case ~ Ageat1stpre + Sex + Filter + Yrs_edu + Age_onset + DUP_days +
-            DUP_DSH_His + DUP_SS_His + sub_abuse,
+m0_fit <- preprocessed_df %>% 
+    glm(formula = sh_case ~ Ageat1stpre + Sex + Filter + Yrs_edu + Age_onset +
+            DUP_days + DUP_DSH_His + DUP_SS_His + sub_abuse + Smoker4 + 
+            baseline_sa + Schiz + EP1_hosp + dur_adm1,
         family = binomial(link="logit"))
 
-m1_fit <- data %>% 
-    glm(formula = sh_case ~ Ageat1stpre + Sex + Filter + Yrs_edu + Age_onset + DUP_days +
-            DUP_DSH_His + DUP_SS_His + sub_abuse + mean_36m_pos + mean_36m_neg +
-            mean_36m_aff + mean_36m_sofas + mean_36m_compliance,
+m1_fit <- preprocessed_df %>% 
+    glm(formula = sh_case ~ Ageat1stpre + Sex + Filter + Yrs_edu + Age_onset +
+            DUP_days + DUP_DSH_His + DUP_SS_His + sub_abuse + Smoker4 + 
+            baseline_sa + Schiz + EP1_hosp + dur_adm1 + 
+            mean_36m_pos + mean_36m_neg + mean_36m_aff + mean_36m_sofas + 
+            mean_36m_compliance,
         family = binomial(link="logit"))
 
-m2_fit <- data %>% 
-    glm(formula = sh_case ~ Ageat1stpre + Sex + Filter + Yrs_edu + Age_onset + DUP_days +
-            DUP_DSH_His + DUP_SS_His + sub_abuse + mssd_pos + mssd_neg +
-            mssd_aff + mssd_sofas + mssd_compliance,
+m2_fit <- preprocessed_df %>% 
+    glm(formula = sh_case ~ Ageat1stpre + Sex + Filter + Yrs_edu + Age_onset +
+            DUP_days + DUP_DSH_His + DUP_SS_His + sub_abuse + Smoker4 + 
+            baseline_sa + Schiz + EP1_hosp + dur_adm1 +
+            mssd_pos + mssd_neg + mssd_aff + mssd_sofas + mssd_compliance,
         family = binomial(link="logit"))
 
 m0_fit_table <- m0_fit %>% 
-    tbl_regression(add_estimate_to_reference_rows = TRUE) %>% 
+    tbl_regression(
+        add_estimate_to_reference_rows = TRUE,
+        label = list(
+            Ageat1stpre ~ "Age (Baseline)",
+            Yrs_edu ~ "Education Years", 
+            Age_onset ~ "Age of Onset",
+            Filter ~ "Treatment",
+            DUP_days ~ "DUP (Days)",
+            DUP_DSH_His ~ "DSH in DUP",
+            DUP_SS_His ~ "SS in DUP",
+            sub_abuse ~ "Substance Abuse (Lifetime)",
+            baseline_sa ~ "Substance Abuse (Baseline)",
+            Smoker4 ~ "Current Smoker",
+            Schiz ~ "Diagnosis",
+            EP1_hosp ~ "Hospitalization at Onset",
+            dur_adm1 ~ "Duration of 1st Admission (Days)"
+        )
+    ) %>% 
     add_q(method = "fdr",
           pvalue_fun = function(x) style_pvalue(x, digits = 2),
           quiet = TRUE) %>% 
-    bold_p(q = TRUE)
+    bold_p(q = TRUE) %>% 
+    modify_footnote(
+        label ~ paste(
+            "Abbreviations:",
+            "DUP = Duration of Untreated Psychosis;",
+            "DSH = Deliberated Self-harm;",
+            "SA = Suicidal Attempts;",
+            "Pos = Positive;",
+            "Neg = Negative;",
+            "Dep = Depressive;",
+            "SOFAS = Social and Occupational Functioning Assessment Scale;",
+            "MSSD = Mean of the Squared Successive Differences",
+            sep = " "
+        )
+    )
 
 m1_fit_table <- m1_fit %>% 
-    tbl_regression(add_estimate_to_reference_rows = TRUE) %>% 
+    tbl_regression(
+        add_estimate_to_reference_rows = TRUE,
+        label = list(
+            Ageat1stpre ~ "Age (Baseline)",
+            Yrs_edu ~ "Education Years", 
+            Age_onset ~ "Age of Onset",
+            Filter ~ "Treatment",
+            DUP_days ~ "DUP (Days)",
+            DUP_DSH_His ~ "DSH in DUP",
+            DUP_SS_His ~ "SS in DUP",
+            sub_abuse ~ "Substance Abuse (Lifetime)",
+            baseline_sa ~ "Substance Abuse (Baseline)",
+            Smoker4 ~ "Current Smoker",
+            Schiz ~ "Diagnosis",
+            EP1_hosp ~ "Hospitalization at Onset",
+            dur_adm1 ~ "Duration of 1st Admission (Days)",
+            mean_36m_pos ~ "Pos. Symptom (36m Mean)",
+            mean_36m_neg ~ "Neg. Symptom (36m Mean)",
+            mean_36m_aff ~ "Dep. Symptom (36m Mean)",
+            mean_36m_sofas ~ "SOFAS (36m Mean)",
+            mean_36m_compliance ~ "Compliance (36m Mean)"
+        )
+    ) %>% 
     add_q(method = "fdr",
           pvalue_fun = function(x) style_pvalue(x, digits = 2),
           quiet = TRUE) %>% 
-    bold_p(q = TRUE)
+    bold_p(q = TRUE) %>% 
+    modify_footnote(
+        label ~ paste(
+            "Abbreviations:",
+            "DUP = Duration of Untreated Psychosis;",
+            "DSH = Deliberated Self-harm;",
+            "SA = Suicidal Attempts;",
+            "Pos = Positive;",
+            "Neg = Negative;",
+            "Dep = Depressive;",
+            "SOFAS = Social and Occupational Functioning Assessment Scale;",
+            "MSSD = Mean of the Squared Successive Differences",
+            sep = " "
+        )
+    )
 
 m2_fit_table <- m2_fit %>% 
-    tbl_regression(add_estimate_to_reference_rows = TRUE) %>% 
+    tbl_regression(
+        add_estimate_to_reference_rows = TRUE,
+        label = list(
+            Ageat1stpre ~ "Age (Baseline)",
+            Yrs_edu ~ "Education Years", 
+            Age_onset ~ "Age of Onset",
+            Filter ~ "Treatment",
+            DUP_days ~ "DUP (Days)",
+            DUP_DSH_His ~ "DSH in DUP",
+            DUP_SS_His ~ "SS in DUP",
+            sub_abuse ~ "Substance Abuse (Lifetime)",
+            baseline_sa ~ "Substance Abuse (Baseline)",
+            Smoker4 ~ "Current Smoker",
+            Schiz ~ "Diagnosis",
+            EP1_hosp ~ "Hospitalization at Onset",
+            dur_adm1 ~ "Duration of 1st Admission (Days)",
+            mssd_pos ~ "Pos. Symptom (MSSD)",
+            mssd_neg ~ "Neg. Symptom (MSSD)",
+            mssd_aff ~ "Dep. Symptom (MSSD)",
+            mssd_sofas ~ "SOFAS (MSSD)",
+            mssd_compliance ~ "Compliance (MSSD)"
+        )
+    ) %>% 
     add_q(method = "fdr",
           pvalue_fun = function(x) style_pvalue(x, digits = 2),
           quiet = TRUE) %>% 
-    bold_p(q = TRUE)
+    bold_p(q = TRUE) %>% 
+    modify_footnote(
+        label ~ paste(
+            "Abbreviations:",
+            "DUP = Duration of Untreated Psychosis;",
+            "DSH = Deliberated Self-harm;",
+            "SA = Suicidal Attempts;",
+            "Pos = Positive;",
+            "Neg = Negative;",
+            "Dep = Depressive;",
+            "SOFAS = Social and Occupational Functioning Assessment Scale;",
+            "MSSD = Mean of the Squared Successive Differences",
+            sep = " "
+        )
+    )
 
-tbl_merge(
+table_regresson_all_models <- tbl_merge(
     tbls = list(m0_fit_table, m1_fit_table, m2_fit_table),
     tab_spanner = c("**M0**", "**M1**", "**M2**")
-) %>%
+) 
+
+table_regresson_all_models %>%
     as_gt() %>% 
     gt::gtsave(filename = here("outputs", "tables", "data-cf_desc-models_comparison.html"))
 
-# check model fitness
+table_regresson_all_models %>%
+    as_flex_table() %>% 
+    flextable::bold(bold = TRUE, part = "header") %>% 
+    flextable::fontsize(size = 10, part = "all") %>% 
+    flextable::autofit() %>% 
+    flextable::save_as_docx(
+        path = here("outputs", "tables", "data-cf_desc-models_comparison.docx"),
+        pr_section = sect_properties
+    )
+
+
+
+ # check model fitness
 summ(m0_fit)
 summ(m1_fit)
 summ(m2_fit)
@@ -67,3 +194,4 @@ summ(m2_fit)
 anova(m0_fit, m1_fit, test = "Chisq")
 anova(m0_fit, m2_fit, test = "Chisq")
 anova(m1_fit, m2_fit, test = "Chisq")
+
