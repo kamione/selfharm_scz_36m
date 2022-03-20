1e926490ee29b45e86823cb715f4112c702919a2
Ting-Yat Wong <yatyat0321@gmail.com>
2022-03-20 18:03:54 -0400

    [fixed] filenames

--- scripts/09_a_prediction_performance.R
+++ scripts/09_a_prediction_performance.R
@@ -37,7 +37,7 @@ boxplot_rf_12mto12m <- plot_timeseries_boxplots(results_mat_rf_12mto12m)
 
 ggarrange(boxplot_rf_6mto6m, boxplot_rf_6mto12m, boxplot_rf_12mto6m, boxplot_rf_12mto12m,
           ncol = 2, nrow = 2, labels = LETTERS[1:4]) %>% 
-    ggexport(filename = here("outputs", "figs", "summary_boxplots_model-rf_desc_prediction.pdf"),
+    ggexport(filename = here("outputs", "figs", "summary_boxplots_data-cf_model-rf_desc_prediction.pdf"),
              width = 14, height = 9)
 
 # Tables -----------------------------------------------------------------------
