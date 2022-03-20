b86513b37ffbdbd26d766029bb2f8473f6762cea
Ting-Yat Wong <yatyat0321@gmail.com>
2022-03-19 20:11:02 -0400

    [fixed] figure visualization

--- scripts/03_a_tvmvar_cf.R
+++ scripts/03_a_tvmvar_cf.R
@@ -6,13 +6,17 @@ library(mgm)
 library(qgraph)
 library(ggthemes)
 
-# Data I/O ---------------------------------------------------------------------
-data <- here("data", "processed", "cf_selfharm_longitudinal.csv") %>% 
-    read_csv(col_types = cols())
 
+# Data IO  ---------------------------------------------------------------------
+preprocessed_df <- here("data", "processed", "cf_selfharm_longitudinal.rds") %>% 
+    read_rds()
+
+
+# Visualization Time x Symptom Severity ----------------------------------------
 # prepare data frame for analysis
-data_long <- data %>% 
-    mutate(index = 1:1207, .before = "sh_case") %>% 
+preprocessed_df_long <- preprocessed_df %>%
+    mutate(index = 1:n(), .before = sh_case) %>% 
+    rename_with(~str_replace(., "Funct_scorecf", "Functscorecf")) %>% 
     select(index,
            sh_case,
            M1_Poscf:M36_Poscf,
@@ -22,29 +26,27 @@ data_long <- data %>%
            M1_compliance:M36_compliance,
            M1_case:M36_case
     ) %>% 
-    zap_labels() %>% 
     pivot_longer(
         !c(index, sh_case),
         names_to = c("month", "measure"),
         names_pattern = "M(.*)_(.*)",
         values_to = "value"
-    ) %>% 
+    ) %>%
     mutate(month = as.numeric(month)) %>% 
     pivot_wider(
         names_from = measure,
         values_from = value
-    ) %>%
-    drop_na()
+    )
 
-longitudial_change_fig <- data_long %>% 
+longitudial_change_fig <- preprocessed_df_long %>% 
     rename(
         "Positive Symptom" = "Poscf",
         "Negative Symptom" = "Negcf",
-        "Affective Symptom" = "Aff"
+        "Depressive Symptom" = "Aff"
     ) %>% 
     # mutate(case = factor(case, levels = c(0, 1), labels = c("No", "Yes"))) %>% 
     pivot_longer(
-        cols = `Positive Symptom`:`Affective Symptom`,
+        cols = `Positive Symptom`:`Depressive Symptom`,
         names_to = "symptom",
         values_to = "severity"
     ) %>% 
@@ -58,7 +60,7 @@ longitudial_change_fig <- data_long %>%
         scale_x_continuous(breaks = c(0, seq(4, 36, 4))) +
         scale_color_manual(values = c("tomato3", "darkgrey", "#E69F00")) +
         scale_fill_manual(values = c("tomato3", "darkgrey", "#E69F00")) +
-        theme_pander() +
+        ggthemes::theme_pander() +
         theme(
             plot.margin = margin(2, 2, 2, 2, "mm"),
             legend.position = "right",
@@ -71,16 +73,20 @@ longitudial_change_fig %>%
            width = 6)
 
 
+# Time-varying Mixed Vector Autoregressive Model -------------------------------
 
+# create dataframe
 tvmvar_data <- NULL
-
-tvmvar_data$data <- data_long %>% select(Poscf:case)
-tvmvar_data$type <- c("g", "g", "g", "g", "g", "c")
+tvmvar_data$data <- preprocessed_df_long %>% select(Poscf:case) %>% drop_na()
+tvmvar_data$type <- c("g", "g", "g", "g", "g","c")
 tvmvar_data$level <- c(1, 1, 1, 1, 1, 2)
-tvmvar_data$timepoint <- data_long %>% select(index, month)
+tvmvar_data$timepoint <- preprocessed_df_long %>%
+    select(index, month, Poscf:case) %>%
+    drop_na() %>%
+    select(index, month)
 
 p <- ncol(tvmvar_data$data)
-n <- nrow(tvmvar_data$data)
+num_row <- nrow(tvmvar_data$data)
 
 set.seed(1111)
 bw_object <- bwSelect(data = tvmvar_data$data,
@@ -93,11 +99,13 @@ bw_object <- bwSelect(data = tvmvar_data$data,
                       lags = 1,
                       scale = TRUE,
                       beepvar = tvmvar_data$timepoint$month,
-                      dayvar = rep(1, nrow(tvmvar_data$data)),
+                      dayvar = rep(1, num_row),
                       pbar = TRUE)
 
-bandwidth <- as.numeric(names(which.min(bw_object$meanError)))
+# save cache
+write_rds(bw_object, here("cache", "bw_object.rds"))
 
+bandwidth <- as.numeric(names(which.min(bw_object$meanError)))
 
 set.seed(1111)
 fit_tvmvar <- tvmvar(data = tvmvar_data$data,
@@ -112,15 +120,15 @@ fit_tvmvar <- tvmvar(data = tvmvar_data$data,
                      threshold = "none",
                      scale = TRUE)
 
+# save cache
+write_rds(fit_tvmvar, here("cache", "fit_tvmvar.rds"))
 
-pred_obj <- predict(object = fit_tvmvar, 
-                    data = tvmvar_data$data, 
-                    tvMethod = "weighted", 
-                    consec = tvmvar_data$timepoint$month)
-pred_obj$errors
-pred_obj$tverrors
-
-fit_tvmvar
+#pred_obj <- predict(object = fit_tvmvar, 
+#                    data = tvmvar_data$data, 
+#                    tvMethod = "weighted", 
+#                    consec = tvmvar_data$timepoint$month)
+#pred_obj$errors
+#pred_obj$tverrors
 
 
 wadj_av <- matrix(apply(fit_tvmvar$wadj, 1:3, mean), 6, 6)
@@ -132,8 +140,6 @@ qgraph(t(fit_tvmvar$wadj[, , 1, 3]),
        nodeNames = colnames(tvmvar_data$data), 
        legend = F)
 
-
-
 # Prepare Edges over time
 wadj_w_sign <- fit_tvmvar$wadj * fit_tvmvar$signs
 wadj_w_sign[is.na(wadj_w_sign)] <- 0
@@ -142,82 +148,85 @@ n_estpoints <- 35
 
 # Define lists of parameters I would like to see over time
 l_es <- list()
-l_es[[1]] <- fit_tvmvar$wadj[1, 6, 1, ] # Worrying -> Worrying
-l_es[[2]] <- fit_tvmvar$wadj[2, 6, 1, ] # Tired -> Tired
-l_es[[3]] <- fit_tvmvar$wadj[3, 6, 1, ] # Selflike -> Down
-l_es[[4]] <- fit_tvmvar$wadj[4, 6, 1, ] # Concentration -> Down
-l_es[[5]] <- fit_tvmvar$wadj[5, 6, 1, ] # Selflike -> Ashamed
-
+l_es[[1]] <- fit_tvmvar$wadj[1, 6, 1, ] # Positive Symptom
+l_es[[2]] <- fit_tvmvar$wadj[2, 6, 1, ] # Negative Symptom
+l_es[[3]] <- fit_tvmvar$wadj[3, 6, 1, ] # Depressive Symptom
+l_es[[4]] <- fit_tvmvar$wadj[4, 6, 1, ] # SOFAS
+l_es[[5]] <- fit_tvmvar$wadj[5, 6, 1, ] # Compliance
 
 
 # Visualization ----------------------------------------------------------------
-
 pdf(here("outputs", "figs", "data-cf_desc-tvmvar_lag1.pdf"), width = 12, height = 8)
 
+# ---------- 0) Set up Layout ----------
 # (0) set up Layout
 lmat <- matrix(c(1, 2, 3, 4,
                  5, 5, 5, 5,
                  6, 6, 6, 6), ncol = 4, byrow = T)
-lo <- layout(lmat, heights = c(1, 1.1, 0.2))
+lo <- layout(lmat, heights = c(1, 1.5, 0.1))
 # layout.show(lo)
 
+# ---------- b) Three Network Plots ----------
 # (1) display selected graphs
 E_select <- c(5, 13, 21, 29)
-
 for(tp in E_select) {
     qgraph(fit_tvmvar$wadj[, , 1, tp], 
            edge.color = fit_tvmvar$edgecolor[, , 1, tp],
            border.color = "gray60",
            layout = Q$layout, 
-           labels = c("POS", "NEG", "AFF", "SOFAS", "COMP", "DSH"), 
+           labels = c("POS", "NEG", "DEP", "SOFAS", "COMP", "DSH"), 
            legend = FALSE,
            vsize = 13, 
            esize = 10,
+           edge.width = 1.5,
            asize = 10, 
-           mar = c(6, 6, 6, 6),
+           mar = c(4, 10, 10, 10),
            fade = FALSE,
-           minimum = 0.05, 
+           minimum = 0.1, 
            maximum = 1)
+    title(glue("Month = {tp + 1}"))
 }
 
 
 # ---------- c) A couple of edges over time ----------
-
 col_es <- RColorBrewer::brewer.pal(5, 'Set1')
 
 plot.new()
-par(mar = c(1,7,1,7))
-plot.window(xlim=c(0,1), ylim=c(-.2,.4))
+par(mar = c(0, 7, 1, 3))
+plot.window(xlim = c(1, 35), ylim = c(-0.2, 0.6))
 
-abline(h = 0, lty=2, col='grey')
+abline(h = 0, lty = 3, col = "grey40")
 
 for(i in 1:5) {
-    lines(seq(0, 1, length = n_estpoints), l_es[[i]], col=col_es[i], lwd=1.5, lty=1)
+    lines(1:n_estpoints, l_es[[i]], col = col_es[i], lwd = 4, lty = 1)
     # points(seq(0, 1, length = n_estpoints), l_es[[i]], col='white', pch=20, cex = 1.5)
     # points(seq(0, 1, length = n_estpoints), l_es[[i]], col=col_es[i], pch=20)
 }
 
-# Add axis
-axis(2, seq(-2, 4, 0.05), las=2)
-title(ylab = 'Parameter value', line =3.5)
+# add axis
+axis(2, seq(-0.2, 0.45, 0.05), las = 2)
+title(ylab = "Parameter Value", line = 3.5, cex.lab = 1.5)
 
-# Legend
-legend_labels <- c(expression("POS"["t-1"]  %->%  "DSH/SS"["t"]),
-                   expression("NEG"["t-1"]  %->%  "DSH/SS"["t"]),
-                   expression("AFF"["t-1"]  %->%  "DSH/SS"["t"]))
-legend(0.5, -.05, legend_labels, lwd = 2, col = col_es[1:3], bty = 'n', cex = 1.3)
-legend_labels <- c(expression("SOFAS"["t-1"]  %->%  "DSH/SS"["t"]),
-                   expression("COMPLIENCE"["t-1"]  %->%  "DSH/SS"["t"]))
-legend(0.7, -.05, legend_labels, lwd = 2, col = col_es[4:5], bty = 'n', cex = 1.3)
+# add legend
+legend_labels <- c(expression("POS"["t-1"]  %->%  "DSH"["t"]),
+                   expression("NEG"["t-1"]  %->%  "DSH"["t"]),
+                   expression("DEP"["t-1"]  %->%  "DSH"["t"]))
+legend(22, -0.05, legend_labels, lwd = 2, col = col_es[1:3], bty = "n", cex = 1.5)
+legend_labels <- c(expression("SOFAS"["t-1"]  %->%  "DSH"["t"]),
+                   expression("COMP"["t-1"]  %->%  "DSH"["t"]))
+legend(29, -0.05, legend_labels, lwd = 2, col = col_es[4:6], bty = "n", cex = 1.5)
 
 
-
-# Estimation points
-x_seq <- seq(0, 1, length = n_estpoints)
+# add estimation points
+x_seq <- 0:n_estpoints
 y_dash <- .01
-segments(x_seq, .4 - y_dash, x_seq, .4 + y_dash, col = "tomato3")
+segments(x_seq, 0.45 - y_dash, x_seq, 0.45 + y_dash, col = "tomato3")
+
+# add arrows
+g_location <- c(2, 12.5, 22.5, 33)
+for (i in 1:4) {
+    shape::Arrows(g_location[i], 0.6, E_select[i], 0.47, col = "gray30", 
+                  lwd = 2.5, arr.adj = 1, arr.type = "triangle")
+}
 
 dev.off()
-
-
-
